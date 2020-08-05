07aa6cda3e76e97bb735df946d384b417213c4d2
maaqib121 <maaqib121@gmail.com>
2020-08-05 17:49:56 +0500

    feat: articles CRUD completed

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,52 @@
+class ArticlesController < ApplicationController
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = "Article successfully created"
+      redirect_to @article
+    else
+      flash[:error] = "Something went wrong"
+      render "new"
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was successfully updated"
+      redirect_to @article
+    else
+      flash[:error] = "Something went wrong"
+      render "edit"
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    if @article.destroy
+      flash[:success] = 'Article was successfully deleted.'
+      redirect_to articles_path
+    else
+      flash[:error] = 'Something went wrong'
+      redirect_to objects_url
+    end
+  end
+  
+
+end
