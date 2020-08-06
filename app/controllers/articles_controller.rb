4e7b3031e6b8d8b0d30655a83465d26f575978ba
maaqib121 <maaqib121@gmail.com>
2020-08-06 10:11:02 +0500

    feat: section 4 completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destory]
+
   def show
-    @article = Article.find(params[:id])
   end
 
   def index
@@ -12,11 +13,10 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article successfully created"
       redirect_to @article
@@ -27,8 +27,7 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params)
       flash[:notice] = "Article was successfully updated"
       redirect_to @article
     else
@@ -38,15 +37,18 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
-    if @article.destroy
-      flash[:success] = 'Article was successfully deleted.'
-      redirect_to articles_path
-    else
-      flash[:error] = 'Something went wrong'
-      redirect_to objects_url
-    end
+    @article.destroy
+    flash[:notice] = "Article was successfully deleted."
+    redirect_to articles_path
   end
-  
 
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 end
