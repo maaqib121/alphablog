1c1931725d49cbd528b73d623964a5900f863027
maaqib121 <maaqib121@gmail.com>
2020-08-07 10:29:56 +0500

    refactor: user layout cleaned

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
   def show
     @user = User.find(params[:id])
     @articles = @user.articles
@@ -26,7 +30,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "Your profile is successfully updated"
-      redirect_to articles_path
+      redirect_to @user
     else
       render "edit"
     end
